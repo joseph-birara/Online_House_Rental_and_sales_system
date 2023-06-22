c663c6a3e08d1d1f9e5d6e9e150dc064b0f3344e
Fitsum Gedefaw <gedefawfitsum@gmail.com>
2023-06-22 17:53:09 +0300

    Add filter

---
+++ client/src/pages/TermsOfService.js
@@ -0,0 +1,227 @@
+import React from "react";
+
+const TermsOfService = () => {
+  return (
+    <div>
+      <body>
+        <h1>Terms of Service</h1>
+        <p>
+          <strong>Effective Date:</strong> June 22, 2023
+        </p>
+
+        <p>
+          Welcome to Homiee/All-In-One Online Home Rental and Sales System!
+          These Terms of Service ("Terms") govern your use of our platform, so
+          please read them carefully. By accessing or using our platform, you
+          agree to be bound by these Terms. If you do not agree with any
+          provision of these Terms, you may not use our platform.
+        </p>
+
+        <h2>1. Definitions</h2>
+        <ul>
+          <li>
+            <strong>Platform:</strong> Refers to Homiee or All-In-One Online
+            Home Rental and Sales System, including the website and any related
+            services or features.
+          </li>
+          <li>
+            <strong>User:</strong> Refers to any individual or entity that
+            accesses or uses the platform.
+          </li>
+          <li>
+            <strong>Tenant/Renter:</strong> Refers to a User who is seeking to
+            rent a house through the platform.
+          </li>
+          <li>
+            <strong>/Home-owner/Owner:</strong> Refers to a User who is seeking
+            to rent or sell a house through the platform.
+          </li>
+          <li>
+            <strong>Buyer:</strong> Refers to a User who is seeking to buy or
+            sell a house through the platform.
+          </li>
+          <li>
+            <strong>Listing:</strong> Refers to a house that is listed for rent
+            or sale on the platform.
+          </li>
+        </ul>
+
+        <h2>2. Use of the Platform</h2>
+        <h3>2.1 Eligibility</h3>
+        <p>
+          You must be at least 18 years old and have the legal capacity to enter
+          into contracts to use our platform. By accessing or using the
+          platform, you represent and warrant that you meet these requirements.
+        </p>
+
+        <h3>2.2 Account Registration</h3>
+        <p>
+          To access certain features of the platform, you may be required to
+          create an account. You agree to provide accurate and complete
+          information during the registration process and to keep your account
+          information up to date. You are responsible for maintaining the
+          confidentiality of your account credentials and for all activities
+          that occur under your account. You must notify us immediately of any
+          unauthorized use or security breach of your account.
+        </p>
+
+        <h3>2.3 Prohibited Conduct</h3>
+        <p>
+          You agree not to engage in any of the following prohibited conduct
+          while using our platform:
+        </p>
+        <ul>
+          <li>Violating any applicable law or regulation.</li>
+          <li>
+            Infringing upon the rights of others, including intellectual
+            property rights.
+          </li>
+          <li>
+            Posting, transmitting, or distributing any content that is unlawful,
+            harmful, defamatory, obscene, or otherwise objectionable.
+          </li>
+          <li>
+            Interfering with the proper functioning of the platform or
+            attempting to gain unauthorized access to any system or network.
+          </li>
+          <li>Engaging in any fraudulent or deceptive activities.</li>
+        </ul>
+
+        <h2>3. Listings and Transactions</h2>
+        <h3>3.1 Property Listings</h3>
+        <p>
+          Home-owners can list their homes for rent or sale on the platform. You
+          represent and warrant that you have the legal authority to rent or
+          sell the listed property and allow us to verify the your posession of
+          the homes. You are responsible for providing accurate and up-to-date
+          information about the property, including its description,
+          availability, pricing, and any applicable terms and conditions.
+        </p>
+
+        <h3>3.2 Tenant Obligations</h3>
+        <p>
+          Tenants are responsible for any damage they cause to the home/s they
+          rented and other properties of the home-owner. Incase of any damages
+          to the home/s or other properties of the home-owner, tenants have to
+          compensate the owner based on the terms of service of this platform.
+        </p>
+
+        <h3>3.3 Transactions</h3>
+        <p>
+          Our platform facilitates the initial connection between tenants/buyers
+          and Owners and subsequent communications and interactions
+          between owners and their residents. Hence, any extra negotiations, or transactions
+          between Renters/Buyers and Owners are solely between the parties involved. We
+          are not a party to any rental or sales agreements and do not assume
+          any responsibility or liability for such agreements.
+        </p>
+
+        <h2>4. Fees and Payments</h2>
+        <h3>4.1 Service Fees</h3>
+        <p>
+          We may charge service fees for the use of our platform. The applicable
+          fees will be clearly disclosed to you before you make any payment.
+          Service fees are non-refundable unless otherwise specified in these
+          Terms or required by applicable law.
+        </p>
+
+        <h3>4.2 Payment Processing</h3>
+        <p>
+          We may utilize third-party payment processors to facilitate
+          transactions on the platform. By using our platform, you agree to
+          comply with the terms and conditions of any third-party payment
+          processor. We are not responsible for any actions or omissions of
+          third-party payment processors.
+        </p>
+
+        <h2>5. Intellectual Property</h2>
+        <h3>5.1 Ownership</h3>
+        <p>
+          The platform and all of its content, including but not limited to
+          text, graphics, logos, images, software, and user interfaces, are
+          owned or licensed by us and are protected by copyright, trademark, and
+          other intellectual property laws. You may not use, reproduce, modify,
+          or distribute any content from the platform without our prior written
+          consent.
+        </p>
+
+        <h3>5.2 User Content</h3>
+        <p>
+          By submitting any content to the platform, you grant us a worldwide,
+          royalty-free, non-exclusive license to use, reproduce, modify, adapt,
+          publish, distribute, and display such content for the purpose of
+          providing and promoting the platform. You represent and warrant that
+          you have the necessary rights to grant this license.
+        </p>
+
+        <h2>6. Disclaimers and Limitations of Liability</h2>
+        <h3>6.1 Disclaimers</h3>
+        <p>
+          We provide the platform on an "as is" and "as available" basis,
+          without any warranties or representations, express or implied. We do
+          not warrant that the platform will be error-free, secure, or
+          uninterrupted. Your use of the platform is at your own risk.
+        </p>
+
+        <h3>6.2 Limitation of Liability</h3>
+        <p>
+          To the maximum extent permitted by applicable law, we shall not be
+          liable for any indirect, incidental, consequential, or punitive
+          damages arising out of or in connection with your use of the platform.
+          Our total liability, whether in contract, tort, or otherwise, shall
+          not exceed the fees paid by you, if any, for using the platform.
+        </p>
+
+        <h2>7. Modification of Terms</h2>
+        <p>
+          We reserve the right to modify or update these Terms at any time. If
+          we make any material changes, we will notify you by email or by
+          posting a notice on the platform. Your continued use of the platform
+          after the effective date of the revised Terms constitutes your
+          acceptance of the changes.
+        </p>
+
+        <h2>8. Governing Law and Dispute Resolution</h2>
+        <p>
+          These Terms shall be governed by and construed in accordance with the
+          laws of [Jurisdiction]. Any dispute arising out of or in connection
+          with these Terms shall be subject to the exclusive jurisdiction of the
+          courts located in [Jurisdiction].
+        </p>
+
+        <h2>9. Miscellaneous</h2>
+        <p>
+          These Terms constitute the entire agreement between you and us
+          regarding your use of the platform and supersede any prior agreements
+          or understandings. If any provision of these Terms is found to be
+          invalid or unenforceable, the remaining provisions shall continue to
+          be valid and enforceable. Our failure to enforce any right or
+          provision of these Terms shall not constitute a waiver of such right
+          or provision.
+        </p>
+
+        <p>
+          If you have any questions or concerns about these Terms, please
+          contact us at [insert contact information].
+        </p>
+
+        <p>
+          By using our platform, you acknowledge that you have read, understood,
+          and agree to be bound by these Terms of Service.
+        </p>
+
+        <p>
+          <strong>[Signature]</strong>
+        </p>
+        <p>
+          <strong>All-In-One Online Home Rental and Sales System</strong>
+        </p>
+        <p>
+          <strong>June 22, 2023</strong>
+        </p>
+      </body>
+    </div>
+  );
+};
+
+export default TermsOfService;

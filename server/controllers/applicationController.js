df877fa251a19faba078e15871f1f234ff761334
Dawit Andargachew <hereisdawit@gmail.com>
2023-06-24 21:54:58 +0300

    return type changed

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -12,10 +12,11 @@ const applicationsWithVisitRequest = async (req, res) => {
   try {
     const applications = await applicationModel
       .find({ visitRequest: { $ne: null }, status: "pending" })
-      .populate({ path: "homeId", select: "title city subCity" })
+      .populate({ path: "homeId", select: "woreda subCity" })
       .populate({ path: "applicantId", select: "name phone eamil lastName" })
       .populate({ path: "ownerId", select: "name phone eamil lastName" });
-    return res.status(200).json({ status: "succes", applications });
+    return res.status(200).json(applications);
+
   } catch (error) {
     console.log(error);
     return res.status(201).send("failed");

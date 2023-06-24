8a60a70b79de936701a08e1977d6b070ce3b95d6
Dawit Andargachew <hereisdawit@gmail.com>
2023-06-24 15:04:46 +0300

    alignments pages

--- client/src/Mycomponents/TeamCard.js
+++ client/src/Mycomponents/TeamCard.js
@@ -3,10 +3,10 @@ import React from "react";
 const TeamMemberCard = ({ name, role, description, imageUrl }) => {
   return (
     <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden transform transition duration-300 hover:scale-105">
-      <img className="w-full h-48 object-cover" src={imageUrl} alt={name} />
+      <img className="h-1/4  block rounded-lg mx-auto object-cover" src={imageUrl} alt={name} />
       <div className="py-4 px-6">
         <h2 className="text-lg font-semibold text-gray-800">{name}</h2>
-        <p className="text-sm text-gray-600 mb-2">{role}</p>
+        <p className="text-sm text-gray-600 my-3 pr-4"><hr /></p>
         <p className="text-gray-700 text-base">{description}</p>
       </div>
     </div>

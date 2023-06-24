323822b2b588d6e9a429d91491f6efa903cbc086
Dawit Andargachew <hereisdawit@gmail.com>
2023-06-24 18:48:22 +0300

    helper functions are added

--- client/src/services/HelperFunction.js
+++ client/src/services/HelperFunction.js
@@ -1,5 +1,24 @@
+// capitalize the first word
 export function capitalizeFirstLetter(word) {
     if (!word)
         return "";
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+// for data formating
+export const FormatDate = (date) => {
+    const dateFormatter = new Intl.DateTimeFormat(undefined, {
+        dateStyle: "medium",
+        timeStyle: "short",
+    });
+
+    return dateFormatter.format(Date.parse(date))
+}
+
+// for number formating
+export const NumberFormatter = (number) => {
+    const options = { style: 'currency', currency: 'ETB' };
+    return number.toLocaleString('en-ET', options);
+}
+
+

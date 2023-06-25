789c811b3debaa5fe9d239c08c4976fce51df361
Dawit Andargachew <hereisdawit@gmail.com>
2023-06-25 23:55:31 +0300

    typo error fixed

--- client/src/pages/Auth/LoginPage.jsx
+++ client/src/pages/Auth/LoginPage.jsx
@@ -225,7 +225,7 @@ export default function LoginPage({ isAdmin }) {
           )}
 
           <Link className="underline text-lightBlue text-black" to={"/forgetpassword"}>
-            Forgot passoword
+            Forgot Password
           </Link>
 
           <button

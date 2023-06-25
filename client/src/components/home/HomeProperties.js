f3ffb72e182dba07acaede0677bbf89c2a1f3df9
Dawit Andargachew <hereisdawit@gmail.com>
2023-06-25 15:18:43 +0300

    verified status shown in homeDetail part

--- client/src/components/home/HomeProperties.js
+++ client/src/components/home/HomeProperties.js
@@ -41,7 +41,25 @@ const HomeProperties = ({ className, specificHouse }) => {
             <p className={styles.prop}>Bathrooms: </p> <p>{specificHouse.bathRoom}</p>
           </div>
           <div className={styles.houseProp}>
-            <p className={styles.prop}>Home Status: </p> <p>{specificHouse.homeStatus}</p>
+            <p className={styles.prop}>Home Status: </p>
+            <p>
+
+              <svg
+                className={`w-4 h-4 scale-125 mr-1.5 ${specificHouse.homeStatus ? 'text-[#38A169]' : 'text-[#DC2626]'}   dark:text-green-400 flex-shrink-0`}
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="m-0 p-o text-base"> {specificHouse.homeStatus ? 'Verified' : 'Not Verified'} </span>
+            </p>
+
+
           </div>
 
           <div className={styles.houseProp}>

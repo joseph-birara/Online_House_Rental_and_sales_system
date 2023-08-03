e548e65f03ebcd2d6477ed83496d778dae01b600
Dawit Andargachew <hereisdawit@gmail.com>
2023-08-03 14:17:39 +0300

    ready to be merged

--- server/authController/passwordHash.js
+++
@@ -1,9 +0,0 @@
-// in a separate file (e.g. auth.js)
-const bcrypt = require('bcrypt');
-
-async function hashPassword(password) {
-  const hashedPassword = await bcrypt.hash(password, 10);
-  return hashedPassword;
-}
-
-module.exports = { hashPassword };
